e736d5ca7d59243a29f896fb6aa88bc26a32e2fc
Diana <taaldiana@gmail.com>
2023-03-24 19:08:51 +0200

    adding todo in progress

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import AddUser from "./components-user/Users/AddUser";
 import UsersList from "./components-user/Users/UsersList";
 
+import Todo from "./components-todo/Todo";
+import NewTodo from "./NewTodo-todo/NewTodo";
 function App() {
   const [users, setUsers] = useState([])
 
@@ -23,6 +25,33 @@ function App() {
       <div>
         <AddUser onAddUser={onAddUserHandler}/>
         <UsersList users={users}/>
+      </div>
+  );
+}
+const DUMMY_TODOS = [
+  {
+    id: 'e1',
+    date: new Date(2023, 0, 10),
+    title:'Hinded korda',
+    rank: 'high'
+  }
+  ]
+const Apptodo = () => {
+  const [todos, setTodos] = useState
+  (DUMMY_TODOS)
+
+  const addTodohandler = (todo) => {
+    console.log('In App.js')
+    setTodos((previousTodos ) => {
+      return [todo, ...previousTodos]
+    })
+  }
+  console.log(todos)
+
+  return (
+      <div className="Apptodo">
+        <NewTodo onAddExpense={addTodohandler}></NewTodo>
+        <Todo expenses={todos}></Todo>
       </div>
   );
 }
